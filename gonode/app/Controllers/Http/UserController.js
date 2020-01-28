75dc83ce2cc21c8d28551ff5b6ce23aefa8ccec1
Felipe Bueno <felipebueno181@gmail.com>
2020-01-27 22:49:34 -0300

    Utilizando transactions

--- gonode/app/Controllers/Http/UserController.js
+++ gonode/app/Controllers/Http/UserController.js
@@ -1,4 +1,5 @@
 'use strict'
+const Database = use('Database')
 
 const User = use('App/Models/User')
 
@@ -7,9 +8,12 @@ class UserController {
     const data = request.only(['username', 'email', 'password'])
     const addresses = request.input('addresses')
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction() // transaction para barrar erros
 
-    await user.addresses().createMany(addresses)
+    const user = await User.create(data, trx) // passa no segundo parametro pra captar erro
+    await user.addresses().createMany(addresses, trx) // fica assim pois é uma funcion de callback
+
+    await trx.commit()
 
     return user
   }
